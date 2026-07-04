1f68b27381de31dc4cdb4ea1ea215955ccd9d1bd
Musely <jboysamuel08@gmail.com>
2026-07-04 08:40:31 -0700

    modify script.js

--- randomQuoteGenerator/script.js
+++ randomQuoteGenerator/script.js
@@ -1,26 +1,51 @@
 'use strict';
 
-const quoteCon = getElementById('quote-container');
-const quote = getElementById('quote');
-const author = getElementById('author');
-const twitter = getElementById('twitter');
-const newQuote = getElementById('new-quote');
-const loader = getElementById('loader');
+const quoteCon = document.getElementById('quote-container');
+const quote = document.getElementById('quote');
+const author = document.getElementById('author');
+const twitter = document.getElementById('twitter');
+const newQuote = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 
-const random = Math.floor(Math.random() * 8);
+
+
+
+newQuote.addEventListener('click', newqq);
+
+
+
+  function newqq () {
+
+
+    let random = Math.floor(Math.random() * 14);
 
 if (random <= 2){
   random = 'Dream big. Start small. Stay consistent.';
-}else if (random <= 4){
+}else if (random <= 4 && random >= 2){
   random = 'Your only limit is the one you refuse to challenge.'
-}else if (random <= 6) {
+}else if (random <= 6 && random >= 4) {
   random = 'Success is built one small step at a time.';
-}
+}else if (random <= 8 && random >= 6) {
+  random = 'Progress beats perfection.';
+}else if (random <= 10 && random >= 8) {
+  random = 'Make today count.';
+}else if (random <= 12 && random >= 10) {
+  random = 'You become what you repeatedly do.';
+}else if (random <= 14 && random >= 12) {
+  random = 'Hard work whispers before success shouts.';
+};
+
+    quote.innerHTML = '';
+    author.innerHTML = '';
 
 
-newQuote.onclick = function () {
+  let newQ = document.createElement('p');
+  let auth = document.createElement('p');
 
-  const newQ = document.createElement('p');
+  auth.textContent = 'Musely';
   newQ.textContent = random;
+
+  quote.appendChild(newQ);
+  author.appendChild(auth);
 }
