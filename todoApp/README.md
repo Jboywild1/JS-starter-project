248cfb5f234d5911b79a38f740815f1019536dbf
Musely <jboysamuel08@gmail.com>
2026-07-04 06:40:03 -0700

    Added all the necessary folder

---
+++ todoApp/README.md
@@ -0,0 +1,4 @@
+## Todo List
+
+# Firstly i used the "'use strict' ;" command and then i started calling or saving all my Id's in variables
+Then i added an event listener to my add button so that anytime clicked the button it will run the function
---
+++ todoApp/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const input = document.getElementById("todo-input");
+const addBTn = document.getElementById("add-button");
+const todoList = document.getElementById("todo-list");
+
+// addBTn.onclick = function () {
+//   let newLi = document.createElement('li');
+//   newLi.textContent = input.value, document.createElement('button');
+
+//   todoList.appendChild(newLi);
+// }
+
+addBTn.addEventListener('click', newe);
+
+  function newe () {
+    let newLi = document.createElement('li');
+    let deleteBtn = document.createElement('button');
+  newLi.textContent = input.value;
+  deleteBtn.textContent = 'Delete';
+
+  deleteBtn.addEventListener('click', function () {
+    todoList.removeChild(newLi);
+  });
+
+  newLi.appendChild(deleteBtn);
+  todoList.appendChild(newLi);
+  }
+
+  input.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      addBTn.click();
+    }
+  });
